3213a2c18e7f9d6f3ee803914f464f7d39c529ae
ElTiago <126792630+ElTiago@users.noreply.github.com>
2023-11-29 22:23:52 -0500

    Create Jugador_Main.java

---
+++ Apuntado/Jugador_Main.java
@@ -0,0 +1,47 @@
+package apuntado;
+
+public class Jugador_Main {
+    private Cuenta cuenta;
+    private String nombre;
+    private int fichas;
+    private int dinero;
+
+    public Jugador_Main(String nombre, int edad) {
+        this.cuenta = null;
+        this.nombre = nombre;
+        this.fichas = 0;
+        this.dinero = 0;
+    }
+
+    public void ingresarCuenta() {
+
+    }
+
+    public void cuenta(String nombre, String contraseña, int fichas, int saldo) {
+
+    }
+
+    public void seleccionarMontoPartida() {
+
+    }
+
+    public void comprarFichas() {
+
+    }
+
+    public void retirarDinero() {
+
+    }
+
+    public void leerInstrucciones() {
+
+    }
+
+    public void elegirDiseñoCartas() {
+
+    }
+
+    public void crearPartida() {
+
+    }
+}
