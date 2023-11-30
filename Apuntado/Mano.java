6ba02357e359b7acb5d0fadc657715607249c952
ElTiago <126792630+ElTiago@users.noreply.github.com>
2023-11-29 22:24:35 -0500

    Update Mano.java

--- Apuntado/Mano.java
+++ Apuntado/Mano.java
@@ -1,1 +1,23 @@
+package apuntado;
+import java.util.List;
 
+public class Mano {
+    private List<Carta> cartas;
+    private int puntajeMano;
+    private Jugador jugador;
+
+    public Mano(Jugador jugador) {
+        this.cartas = null;
+        this.puntajeMano = 0;
+        this.jugador = jugador;
+    }
+
+    public int setPuntajeMano() {
+        // Implementa la lógica para calcular el puntaje de la mano
+        return puntajeMano;
+    }
+
+    public String setJugadorMano() {
+        return jugador.getNombre();
+    }
+}
